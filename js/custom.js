2e24058413c23f5f3a9d8cd01dedc52abacc9f1d
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-08-15 12:19:47 -0500

    creating objects out of existing objects

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,14 @@ var person = {
    name: "Steve",
    age: 42,
    favoriteFood: "rats",
-   dislikes: ["turtles", "Spider-Man", "trees", "the Sun"]
+   dislikes: ["turtles", "Spider-Man", "trees", "the Sun"],
+   changeName: function(newName) {
+      if (typeof newName == 'string') {
+         person.name = newName;
+      } else {
+         console.log("Please enter a valid name");
+      }
+   }
 };
 
 console.log(person);
@@ -10,3 +17,8 @@ console.log(person);
 person.dislikes.forEach(function(unwanted) {
    console.log(unwanted);
 });
+
+var bob = Object.create(person);
+
+bob.changeName("Gary");
+console.log(bob);
