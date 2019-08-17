8416abf4b62f529c7e3c703a2425c7f48f81bcf3
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-08-17 14:33:37 -0500

    new constructor via creating a class of object

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,54 @@
-var person = {
-   name: "Steve",
-   age: 42,
-   favoriteFood: "rats",
-   dislikes: ["turtles", "Spider-Man", "trees", "the Sun"],
-   changeName: function(newName) {
-      if (typeof newName == 'string') {
-         person.name = newName;
-      } else {
-         console.log("Please enter a valid name");
-      }
+// var person = {
+//    name: "Steve",
+//    age: 42,
+//    favoriteFood: "rats",
+//    dislikes: ["turtles", "Spider-Man", "trees", "the Sun"],
+//    changeName: function(newName) {
+//       if (typeof newName == 'string') {
+//          person.name = newName;
+//       } else {
+//          console.log("Please enter a valid name");
+//       }
+//    }
+// };
+
+// console.log(person);
+
+// person.dislikes.forEach(function(unwanted) {
+//    console.log(unwanted);
+// });
+
+// var bob = Object.create(person);
+
+// bob.changeName("Gary");
+// console.log(bob);
+
+// function Person(name, age, favoriteFood, dislikes) {
+//    this.name = name;
+//    this.age = age;
+//    this.favoriteFood = favoriteFood;
+//    this.dislikes = dislikes;
+// }
+
+// var anton = new Person("Anton", 23, "Dark chocolate", "Freedom");
+// console.log(anton);
+
+class Person {
+   constructor(name, age, job) {
+      this.name = name;
+      this.age = age;
+      this.job = job;
+      // this.ownsPet = false;
+      // this.likes = ["apples"];
+      // this.dislikes = ["smog"];
    }
-};
 
-console.log(person);
-
-person.dislikes.forEach(function(unwanted) {
-   console.log(unwanted);
-});
-
-var bob = Object.create(person);
-
-bob.changeName("Gary");
-console.log(bob);
-
-function Person(name, age, favoriteFood, dislikes) {
-   this.name = name;
-   this.age = age;
-   this.favoriteFood = favoriteFood;
-   this.dislikes = dislikes;
+   aboutPerson = function() {
+      return "My name is " + this.name + ", I am " + this.age +
+         " years old, and I am a " + this.job;
+   }
 }
 
-var anton = new Person("Anton", 23, "Dark chocolate", "Freedom");
-console.log(anton);
+var tony = new Person("Greg", 19, "construction worker");
+console.log(tony.aboutPerson());
+
